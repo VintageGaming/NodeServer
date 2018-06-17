d2b9658ca12d3e14e195d54a32fe6d481719df4d
James Deaton <jamesdeaton72@gmail.com>
2018-06-17 02:37:19 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,32 +1,15 @@
 var http = require("http");
 var fs = require('fs');
+var express = require('express');
+var app = express();
 
 //Create HTTP server and listen on port 8000 for requests
-http.createServer(function (request, response) {
-	var url = request.url;
-	switch(url) {
-		case '/':
-		    getPageFile(response, 'C:/Users/JamesHammerel/Documents/Websites/Node Server/home.html');
-			break;
-		default:
-		    response.end();
-			break;
-	}
-   
-}).listen(8000);
+
+app.use('/', express.static('./'));
+
+app.get('/', (req, res) => res.sendFile(__dirname+'/home.html'));
+
+app.listen(8000, () => console.log("Port: 8000"));
 
 // Print URL for accessing server
 console.log('Server running at http://127.0.0.1:8000/')
-
-function getPageFile(response, filepath) {
-	fs.readFile(filepath, function(error, data) {
-		if (error) {
-			response.writeHead(500, {'Content-Type':'text/plain'});
-			response.end('500 - Internal Server Error.');
-		}
-		if (data) {
-		    response.writeHead(200, {'Content-Type':'text/html'});
-			response.end(data);
-		}
-	});
-}
